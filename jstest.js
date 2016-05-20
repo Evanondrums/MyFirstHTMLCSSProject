56fdd4a2089ca802b70e84db989a55719b2adc4e
Evanondrums <evanjfarrar7@gmail.com>
2016-05-20 15:58:44 -0700

    Update jstest.js
    added mean and mode functions

--- jstest.js
+++ jstest.js
@@ -5,6 +5,7 @@ function getVals() {
   arr = arr.sort();
   var median = parseInt(arr[arr.length/2]);
   document.getElementById('middiv').innerHTML += ' ' + arr;
+  getDetails();
 }
 
 function getDetails() {
@@ -15,10 +16,28 @@ function getDetails() {
   document.getElementById('middiv').innerHTML += '<br> Mode: ' + mean;
 }
 
-function getMode() {
-  
+function setMode() {
+  var mode = {};
+  var max = 0, count = 0;
+  arr.forEach(function(e) {
+    if (mode[e]) {
+      mode[e]++; 
+    } else { 
+      mode[e] = 1;
+    }
+    if (count < mode[e]) { 
+      max = e;
+      count = mode[e];
+    }
+  });
+  mode = max;
 }
 
-function getMean() {
-
+function setMean() {
+  var sum = 0;
+  for( var i = 0; i < arr.length; i++ ){
+    sum += parseInt( arr[i], 10 );
+  }
+  var avg = sum/arr.length;
+  mean = avg;
 }
